b3c71d69ddb68c3735048463ede06378ef26394e
Gontran Bezerra <gontranbezerra@gmail.com>
2021-04-02 14:44:45 -0300

    feat: Removed errors and build Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,11 @@
+import { useState } from 'react';
 import { GetStaticProps } from 'next';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+import { format } from 'date-fns';
+
+import Head from 'next/head';
+import Prismic from '@prismicio/client';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -16,6 +23,7 @@ interface Post {
 }
 
 interface PostPagination {
+  prev_page: string;
   next_page: string;
   results: Post[];
 }
@@ -24,13 +32,133 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(props: HomeProps): JSX.Element {
+  const { postsPagination } = props;
+  const [postsOfPage, setPostsOfPage] = useState(postsPagination);
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient();
-//   // const postsResponse = await prismic.query(TODO);
+  function loadPosts(url: string): void {
+    try {
+      fetch(url)
+        .then(response => response.json())
+        .then((data: PostPagination) => {
+          const newPosts = data.results.map((post: Post) => {
+            return {
+              uid: post.uid,
+              data: {
+                title: post.data.title,
+                subtitle: post.data.subtitle,
+                author: post.data.author,
+              },
+              first_publication_date: format(
+                new Date(post.first_publication_date),
+                'dd MMM yyyy',
+                {
+                  locale: ptBR,
+                }
+              ),
+            };
+          });
+          setPostsOfPage({
+            next_page: data.next_page,
+            prev_page: data.prev_page,
+            results: newPosts,
+          });
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
-//   // TODO
-// };
+  function handlePreviousPosts(): void {
+    if (postsOfPage.prev_page) {
+      loadPosts(postsOfPage.prev_page);
+    }
+  }
+
+  function handleNextPosts(): void {
+    if (postsOfPage.next_page) {
+      loadPosts(postsOfPage.next_page);
+    }
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+      <div className={commonStyles.container}>
+        <div className={styles.content}>
+          {postsOfPage.results.map(post => (
+            <div key={post.uid}>
+              <h1>{post.data.title}</h1>
+              <h2>{post.data.subtitle}</h2>
+              <div className={styles.info}>
+                <span>
+                  <FiCalendar />
+                  {post.first_publication_date}
+                </span>
+                <span>
+                  <FiUser />
+                  {post.data.author}
+                </span>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className={styles.footer}>
+          <div>
+            {postsOfPage.prev_page && (
+              <button type="button" onClick={handlePreviousPosts}>
+                Carregar posts anteriores
+              </button>
+            )}
+            {postsOfPage.next_page && (
+              <button type="button" onClick={handleNextPosts}>
+                Carregar mais posts
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+  const postsResponse = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      fetch: ['posts.title', 'posts.subtitle', 'posts.author'],
+      pageSize: 5,
+    }
+  );
+
+  const posts = postsResponse.results.map(post => {
+    return {
+      uid: post.uid,
+      data: {
+        title: post.data.title,
+        subtitle: post.data.subtitle,
+        author: post.data.author,
+      },
+      first_publication_date: format(
+        new Date(post.first_publication_date),
+        'dd MMM yyyy',
+        {
+          locale: ptBR,
+        }
+      ),
+    };
+  });
+
+  return {
+    props: {
+      postsPagination: {
+        next_page: postsResponse.next_page,
+        prev_page: postsResponse.prev_page,
+        results: posts,
+      },
+    },
+  };
+};
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,9 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
-import { getPrismicClient } from '../../services/prismic';
+// import { getPrismicClient } from '../../services/prismic';
 
-import commonStyles from '../../styles/common.module.scss';
-import styles from './post.module.scss';
+// import commonStyles from '../../styles/common.module.scss';
+// import styles from './post.module.scss';
 
 interface Post {
   first_publication_date: string | null;
@@ -26,20 +26,58 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post(props: PostProps): JSX.Element {
+  const { post } = props;
+  console.log(post);
+  // TODO
+  return (
+    <div>
+      {/* <div className={commonStyles.banner}>
+        <img src="/images/banner.png" alt="" />
+      </div>
+      <div className={commonStyles.container}>
+        <main className={styles.main}>
+          <section className={styles.sectionContent}>
+            <h1>Criando um app CRA do zero</h1>
+            <span>15 Mar 2021</span>
+            <span>Joseph Oliveira</span>
+            <span>4 min</span>
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+            <p>
+              <h4>Proin et varius</h4>
+              <br />
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              <br />
+              <br />
+              Nullam dolor sapien, vulputate eu diam at, condimentum hendrerit
+              tellus. Nam facilisis sodales felis, pharetra pharetra lectus
+              auctor sed.
+              <br />
+              <br />
+              Ut venenatis mauris vel libero pretium, et pretium ligula
+              faucibus. Morbi nibh felis, elementum a posuere et, vulputate et
+              erat. Nam venenatis.
+            </p>
+          </section>
+        </main>
+      </div> */}
+    </div>
+  );
+}
 
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // const prismic = getPrismicClient();
+//   // const posts = await prismic.query(TODO);
 //   // TODO
 // };
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
-
-//   // TODO
-// };
+export const getStaticProps: GetStaticProps = async context => {
+  // const prismic = getPrismicClient();
+  // const response = await prismic.getByUID(TODO);
+  // TODO
+  return {
+    props: {
+      post: '',
+    },
+  };
+};
